3d63788976779879e6f665c8f68a8402b6d47611
DipaliWalde <122515941+DipaliWalde@users.noreply.github.com>
2023-06-19 01:46:39 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>The Pastry Gallery- About us</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/business-casual.css" rel="stylesheet">
+
+    <!-- Fonts -->
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div class="brand">The Pastry Gallery</div>
+    <div class="address-bar">4/17 Dhake Colony | DN Nagar Metro Station, Andheri West, 400068</div>
+
+    <!-- Navigation -->
+    <?php require_once 'nav.php'; ?>
+
+    <div class="container">
+
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12">
+                    <hr>
+                    <h2 class="intro-text text-center">About
+                        <strong>The Pastry Gallery</strong>
+                    </h2>
+                    <hr>
+                </div>
+                <div class="col-md-6">
+                    <style>
+                        .embed-container {
+                            position: relative;
+                            padding-bottom: 56.25%;
+                            height: 0;
+                            overflow: hidden;
+                            max-width: 100%;
+                        }
+
+                        .embed-container iframe,
+                        .embed-container object,
+                        .embed-container embed {
+                            position: absolute;
+                            top: 0;
+                            left: 0;
+                            width: 100%;
+                            height: 100%;
+                        }
+                    </style>
+                    <div class='embed-container'><iframe src='https://www.youtube.com/embed//lWuTcIBufpo' frameborder='0' allowfullscreen></iframe></div>
+
+                </div>
+                <div class="col-md-6">
+                <div style="font-family: Arial, sans-serif;
+                        font-size: 14.7px;
+                        line-height: 1.5;
+                        text-align: justify;
+                        color: #333333;  width: 100%;
+                        border-collapse: collapse; padding: 0px;
+                          margin: 0;">
+                        <p>The Pastry Gallery was launched in 2021, nestled in the heart of India. Our mission is simple: we aspire to educate pastry enthusiasts worldwide on the art of creating perfect pastries! Our team of expert pastry chefs possesses an unwavering passion for the craft. They have traveled extensively, immersing themselves in diverse culinary traditions and experiencing a multitude of pastry styles. allowing you to embark on a delectable exploration of taste and texture. Our experts are always on hand to provide personalized guidance, answer your questions, and offer tips and tricks that will elevate your pastry-making skills.</p>
+                </div>
+
+                </div>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12">
+                    <hr>
+                    <h2 class="intro-text text-center">About
+                        <strong>Me</strong>
+                    </h2>
+                    <hr>
+                </div>
+                <div class="col-sm-4 text-center">
+                    <img class="img-responsive" src="img/dipuuu.jpeg" alt="">
+                    <h3>Dipali Walde</h3>
+                   
+
+                </div>
+
+                <div class="col-md-8 ">
+                <div style="font-family: Arial, sans-serif;
+                        font-size: 14px;
+                        line-height: 1.5; color:#A5796D;
+                        text-align: justify;
+                        color: #333333;  width: 100%;
+                        border-collapse: collapse; padding: 10px;
+                        border: 2px solid #dddddd;   margin: 0;">
+                        <p>Welcome to The Pastry Gallery, where passion and expertise come together to create delectable delights. My name is Dipali and I am pursuing B.Tech in Electronics Engineering from Bhartiya Vidya Bhavan's Sardar Patel Institute of Technology, Mumbai. Alongside my studies, I have embarked on a side business, driven by my deep love for pastry-making. What began as a hobby and has now transformed into culinary journey, where I pour my heart and soul into crafting exceptional pastries. Thank you everyone for supporting The Pastry Gallery and being a part of our sweet venture. We look forward to delighting your taste buds and creating moments of pure culinary bliss.</p>
+                    </div>
+                </div>
+
+                <div class="clearfix"></div>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container -->
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <p>Copyright &copy; The Pastry Gallery 2023</p>
+                    
+                    
+                </div>
+            </div>
+        </div>
+
+       
+    </footer>
+
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
